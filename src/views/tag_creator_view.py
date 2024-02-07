7de4fe7fe9488c0baa013008fbc5b77a2cbe0a25
Ricardo Pereira <patresio@gmail.com>
2024-02-07 13:22:43 -0300

    feat: Implementing controller and drivers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,3 +1,4 @@
+from src.controllers.tag_creator_controller import TagCreatorController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
@@ -8,11 +9,13 @@ class TagCreatorView:
     '''
 
     def validade_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica da regra de negocios
-        print('Estou na minha view')
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
+
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
